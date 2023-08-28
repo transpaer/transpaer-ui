4e0d31c36525c94ffe9d6575f29dfe289edb7ba6
Wojciech Kluczka <wojciech.kluczka@gmail.com>
2023-08-28 13:34:30 +0200

    Improve sorting of search results

--- sustainity_backend/lib/retrievers.dart
+++ sustainity_backend/lib/retrievers.dart
@@ -19,14 +19,22 @@ class ResultCollector {
 
   ResultCollector() : results = <String, ScoredResult>{};
 
-  void addResults(List<api.SearchResult> items, int? index) {
-    var score = 0.0;
+  // Adds results by giving them some score.
+  //
+  // The score is better if:
+  // - the matched keyword is closer to the beginning of the query
+  // - the matched keyword constitutes the longer part of the whole label
+  void addResults(List<api.SearchResult> items, String match, int? index) {
+    var indexScore = 0.0;
     if (index != null) {
-      score = 1.0 + 1.0 / (index + 1);
+      indexScore = 1.0 / (index + 1);
     } else {
-      score == 10.0;
+      indexScore == 10.0;
     }
     for (final r in items) {
+      var itemScore = match.length / r.label.length;
+      var score = 1.0 + indexScore + itemScore;
+
       results.update(
         r.id,
         (current) => current.withAddedScore(score),
@@ -74,29 +82,36 @@ Future<List<api.SearchResult>> retrieveSearchResults(
   if (matches.length == 1) {
     final lowerCaseMatch = matches[0].toLowerCase();
     final upperCaseMatch = matches[0].toUpperCase();
+
+    // Search organisation by VAT
     {
       final variant = api.SearchResultVariant.organisation;
       final dbItems =
           await client.searchOrganisationsSubstringByVatNumber(upperCaseMatch);
       final apiItems = dbItems.map((r) => r.toApi(variant)).toList();
-      collector.addResults(apiItems, null);
+      collector.addResults(apiItems, "", null);
     }
+
+    // Search product by GTIN
     if (lowerCaseMatch.length < 15) {
       final variant = api.SearchResultVariant.product;
       final match = lowerCaseMatch.padLeft(14, '0');
       final dbItems = await client.searchProductsSubstringByGtin(match);
       final apiItems = dbItems.map((r) => r.toApi(variant)).toList();
-      collector.addResults(apiItems, null);
+      collector.addResults(apiItems, "", null);
     }
+
+    // Search organisation by website
     {
       final variant = api.SearchResultVariant.organisation;
       final dbItems =
           await client.searchOrganisationsSubstringByWebsite(lowerCaseMatch);
       final apiItems = dbItems.map((r) => r.toApi(variant)).toList();
-      collector.addResults(apiItems, null);
+      collector.addResults(apiItems, "", null);
     }
   }
 
+  // Search organisations and products by keyword
   final lowercaseMatches = matches.map((match) => match.toLowerCase()).toList();
   {
     final variant = api.SearchResultVariant.organisation;
@@ -104,7 +119,7 @@ Future<List<api.SearchResult>> retrieveSearchResults(
     for (final match in lowercaseMatches) {
       final dbItems = await client.searchOrganisationsExactByKeyword(match);
       final apiItems = dbItems.map((r) => r.toApi(variant)).toList();
-      collector.addResults(apiItems, index);
+      collector.addResults(apiItems, match, index);
       index += 1;
     }
   }
@@ -114,7 +129,7 @@ Future<List<api.SearchResult>> retrieveSearchResults(
     for (final match in lowercaseMatches) {
       final dbItems = await client.searchProductsExactByKeyword(match);
       final apiItems = dbItems.map((r) => r.toApi(variant)).toList();
-      collector.addResults(apiItems, index);
+      collector.addResults(apiItems, match, index);
       index += 1;
     }
   }
