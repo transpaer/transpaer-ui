500ebf7b7899df49cfadc2771afd5ce08225f548
Wojciech Kluczka <wojciech.kluczka@gmail.com>
2025-08-30 21:01:06 +0200

    Fix the CORS issue with images in the web version

---
+++ transpaer_frontend/lib/cors_image_web.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'package:web/web.dart' as web;
+
+class CorsImage extends StatelessWidget {
+  final String src;
+  final int width;
+  final int height;
+
+  const CorsImage({
+    required this.src,
+    required this.width,
+    required this.height,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width as double,
+      height: height as double,
+      child: Center(
+        child: HtmlElementView.fromTagName(
+          tagName: 'img',
+          onElementCreated: (imgElement) {
+            imgElement as web.HTMLElement;
+            imgElement.setAttribute('src', src);
+            imgElement.style.width = "";
+            imgElement.style.height = "";
+            imgElement.style.maxWidth = "${width.toString()}px";
+            imgElement.style.maxHeight = "${height.toString()}px";
+          },
+        ),
+      ),
+    );
+  }
+}
