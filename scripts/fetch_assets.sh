b0388d4ff9fc0cbffa32657f89bfdef1b03d415e
Wojciech Kluczka <wojciech.kluczka@gmail.com>
2025-08-31 12:33:28 +0200

    Move some library topics to assets
    This will reduce need to edit api files when articles not related to the data were changed.

--- scripts/fetch_assets.sh
+++ scripts/fetch_assets.sh
@@ -5,5 +5,5 @@ DOWNLOADS="https://github.com/transpaer/transpaer-ui-assets/releases/download"
 PACKAGE="transpaer_assets-$VERSION.zip"
 RELEASE="$DOWNLOADS/v$VERSION/$PACKAGE"
 
-cd transpaer_frontend && rm -rf images && wget -q $RELEASE && unzip -q $PACKAGE && rm $PACKAGE
+cd transpaer_frontend/assets && rm -rf images && wget -q $RELEASE && unzip -q $PACKAGE && rm $PACKAGE
 
